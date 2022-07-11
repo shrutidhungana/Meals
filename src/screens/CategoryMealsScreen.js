ed0c7a5bb0506791e7e1ca822014344b1d5564d9
shrutidhungana <shrutidhungana@prixa.net>
2022-07-11 12:11:47 +0545

    Second commit

---
+++ src/screens/CategoryMealsScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+const CategoryMealsScreen = () => {
+  return (
+      <View>
+          <Text>
+          </Text>
+      </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default CategoryMealsScreen
---
+++ src/screens/FavoritesScreen.js
@@ -0,0 +1,20 @@
+import React from 'react' 
+import {View, Text, StyleSheet} from 'react-native'
+
+
+const FavoritesScreen = () => {
+  return (
+      <View>
+          <Text>
+              
+          </Text>
+      </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+
+})
+
+export default FavoritesScreen
